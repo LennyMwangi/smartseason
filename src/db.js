bbe16092e24b6f78bdfdf49d821ce44ebc51d48f
Lenny Mwangi <mwangileny18@gmail.com>
2026-04-25 21:33:24 +0300

    Fix db connection for production

--- src/db.js
+++ src/db.js
@@ -1,12 +1,25 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'smartseason',
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // 🌍 Production (Render)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  // 💻 Local development
+  pool = new Pool({
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'smartseason',
+  });
+}
 
 module.exports = pool;
